fac999b1c04481a165a7ee81b883adb96af10ce4
Max Chu <maxmchu@gmail.com>
2019-01-09 21:31:55 -0500

    Material UI added

---
+++ src/components/login.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import '../styles/App.scss';
+
+class Login extends React.Component {
+  public render() {
+    return (
+      <div>
+        <h1>Test!</h1>
+      </div>
+    )
+  }
+}
+
+export default Login;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import './styles/index.scss';
 import App from './App';
+import store from './redux/index';
 import * as serviceWorker from './serviceWorker';
+import 'typeface-roboto';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  ,
+  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
