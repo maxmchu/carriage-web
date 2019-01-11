7718de2b96bf71bae06ba58c9761cd6809a3275f
Max Chu <maxmchu@gmail.com>
2019-01-11 17:18:30 -0500

    login screen before backend

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,11 +1,32 @@
 import * as React from 'react';
 import '../styles/App.scss';
+import './login.scss';
+
+import { Grid, Paper, Typography } from '@material-ui/core';
 
 class Login extends React.Component {
   public render() {
     return (
       <div>
-        <h1>Test!</h1>
+        <Grid container>
+          <Grid item md={4}>
+            <div className={"login-background"} />
+          </Grid>
+          <Grid item md={8} className={"login-container"}>
+            <Grid container
+              direction={"column"}
+              justify={"center"}
+              alignItems={"flex-start"}>
+              <Grid item md={12}>
+                <Typography variant={"h2"}>ADALift</Typography>
+                <Typography variant={"h5"}>Rider Login</Typography>
+              </Grid>
+              <Grid item md={12}>
+
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
       </div>
     )
   }
