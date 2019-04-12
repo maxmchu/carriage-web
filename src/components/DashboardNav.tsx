8259555dc4d26a714a93221896b4f9bb40fd4236
Max Chu <maxmchu@gmail.com>
2019-04-11 20:30:29 -0400

    Responsive navigation

--- src/components/DashboardNav.tsx
+++ src/components/DashboardNav.tsx
@@ -2,10 +2,9 @@ import * as React from 'react';
 import '../styles/App.scss';
 import { connect } from 'react-redux';
 
-import { Menu, Icon, Image } from 'semantic-ui-react';
+import { Menu, Icon, Image, Responsive, Dropdown, DropdownItem } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import { AccountType } from '../types';
-import Profile from '../containers/Profile';
 
 interface IDashboardNavProps {
   firstName: string;
@@ -13,55 +12,95 @@ interface IDashboardNavProps {
 }
 
 interface IDashboardNavState {
-
+  width: number;
 }
 
 class DashboardNavigation extends React.Component<IDashboardNavProps, IDashboardNavState> {
 
   public constructor(props) {
     super(props);
+    this.state = {
+      width: document.documentElement.clientWidth
+    }
+    this.updateWidth = this.updateWidth.bind(this);
   }
 
-  public render() {
-    return (
-      <Menu icon='labeled'>
-        <Menu.Item className="middle-align">
-          <Image src={require("../assets/carriage.svg")} size='tiny' />
-        </Menu.Item>
-        {
-          (this.props.accountType === AccountType.DISPATCHER) ?
-            <Menu.Item as={Link} to={"/dashboard"} >
-              <Icon name='dashboard' />
-              Dashboard
-            </Menu.Item> :
-            <Menu.Item as={Link} to={"/dashboard"} >
-              <Icon name='car' />
-              Rides
-            </Menu.Item>
-        }
+  public componentWillMount() {
+    this.updateWidth();
+  }
 
-        <Menu.Item as={Link} to={"/dashboard/profile"}>
-          <Icon name='user circle' />
-          Profile
-        </Menu.Item>
-        {
-          (this.props.accountType === AccountType.DRIVER) ?
-            <Menu.Item as={Link} to={"/dashboard/hours"}>
-              <Icon name='clock outline' />
-              Shift hours
-            </Menu.Item> : null
-        }
-        <Menu.Menu position='right'>
-          <Menu.Item className='middle-align'>
-            <p>Logged in as <span style={{ fontWeight: "bold" }}>{this.props.firstName}</span></p>
+  public componentDidMount() {
+    window.addEventListener("resize", this.updateWidth);
+  }
+
+  public componentWillUnmount() {
+    window.removeEventListener("resize", this.updateWidth);
+  }
+
+  // TODO: Fix responsiveness
+  public render() {
+    return (this.state.width >= 768) ?
+      (
+        <Responsive as={Menu} icon='labeled' minWidth={768}>
+          <Menu.Item className="middle-align" as={Link} to={"/dashboard"}>
+            <Image src={require("../assets/carriage.svg")} size='tiny' />
           </Menu.Item>
-          <Menu.Item as={Link} to={"/logout"}>
-            <Icon name='log out' />
-            Log out
+          {
+            (this.props.accountType === AccountType.DISPATCHER) ?
+              <Menu.Item as={Link} to={"/dashboard"} icon='dashboard' content='Dashboard' /> :
+              <Menu.Item as={Link} to={"/dashboard"} icon='car' content='Rides' />
+          }
+          <Menu.Item as={Link} to={"/dashboard/profile"} icon='user circle' content='Profile' />
+          {
+            (this.props.accountType === AccountType.DRIVER) ?
+              <Menu.Item as={Link} to={"/dashboard/hours"} icon='clock outline' content='Shift hours' /> :
+              null
+          }
+          <Menu.Menu position='right'>
+            <Menu.Item className='middle-align'>
+              <p>Logged in as <span style={{ fontWeight: "bold" }}>{this.props.firstName}</span></p>
+            </Menu.Item>
+            <Menu.Item as={Link} to={"/logout"} icon='log out' content='Log out' />
+          </Menu.Menu>
+        </Responsive>
+      )
+      : (
+        <Responsive as={Menu} maxWidth={767}>
+          <Menu.Item className="middle-align" as={Link} to={"/dashboard"}>
+            <Image src={require("../assets/carriage.svg")} size='tiny' />
           </Menu.Item>
-        </Menu.Menu>
-      </Menu>
-    );
+          <Menu.Menu position='right'>
+            <Menu.Item className='middle-align'>
+              <p>Logged in as <span style={{ fontWeight: "bold" }}>{this.props.firstName}</span></p>
+            </Menu.Item>
+            <Dropdown icon='bars' item>
+              <Dropdown.Menu>
+                {
+                  (this.props.accountType === AccountType.DISPATCHER) ?
+                    <Dropdown.Item as={Link} to={"/dashboard"} icon='dashboard' text='Dashboard' /> :
+                    <Dropdown.Item as={Link} to={"/dashboard"} icon='car' text='Rides' />
+                }
+                <Dropdown.Item as={Link} to={"/dashboard/profile"} icon='user circle' text='Profile' />
+                {
+                  (this.props.accountType === AccountType.DRIVER) ?
+                    <Dropdown.Item as={Link} to={"/dashboard/hours"} icon='clock outline' text='Shift hours' /> :
+                    null
+                }
+                <Dropdown.Item as={Link} to={"/logout"} icon='log out' text='Log out' />
+              </Dropdown.Menu>
+            </Dropdown>
+          </Menu.Menu>
+        </Responsive>
+      )
+
+
+
+  }
+
+  private updateWidth() {
+    this.setState({
+      width: document.documentElement.clientWidth
+    });
   }
 
 }
